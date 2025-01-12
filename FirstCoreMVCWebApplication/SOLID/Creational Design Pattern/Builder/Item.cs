eed5d153578c46196d498088d7b354e749e71324
Atikul1213 <atikul.islam@brainstation-23.com>
2025-01-12 19:00:23 +0600

    add builder pattern

---
+++ FirstCoreMVCWebApplication/SOLID/Creational Design Pattern/Builder/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace FirstCoreMVCWebApplication.SOLID.Creational_Design_Pattern.Builder
+{
+    public class Item
+    {
+        public string value1 { get; set; }
+        public string value2 { get; set; }
+        public string value3 { get; set; }
+    }
+}
---
+++ FirstCoreMVCWebApplication/SOLID/Creational Design Pattern/Builder/ItemBuilder.cs
@@ -0,0 +1,31 @@
+﻿namespace FirstCoreMVCWebApplication.SOLID.Creational_Design_Pattern.Builder
+{
+    public class ItemBuilder
+    {
+        private Item _item;
+        public ItemBuilder()
+        {
+            _item = new Item();
+        }
+
+        public void SetValue1(string itemValue)
+        {
+            _item.value1 = itemValue;
+        }
+
+        public void SetValue2(string itemValue)
+        {
+            _item.value2 = itemValue;
+        }
+
+        public void SetValue3(string itemValue)
+        {
+            _item.value3 = itemValue;
+        }
+
+        public Item GetItem()
+        {
+            return _item;
+        }
+    }
+}
