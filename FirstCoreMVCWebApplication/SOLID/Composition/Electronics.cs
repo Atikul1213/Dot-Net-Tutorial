aabc9e9f5f5900643ddaae5e0495be561a99387a
Atikul1213 <atikul.islam@brainstation-23.com>
2025-01-12 12:11:03 +0600

    add composition

---
+++ FirstCoreMVCWebApplication/SOLID/Composition/Electronics.cs
@@ -0,0 +1,25 @@
+﻿namespace FirstCoreMVCWebApplication.SOLID.Composition
+{
+    public class Electronics
+    {
+        public string Name { get; set; }
+        private Product Product { get; set; }
+
+        public double Price
+        {
+            get
+            {
+                return Product.Price;
+            }
+            set
+            {
+                Product.Price = value;
+            }
+        }
+
+        public double GetPriceAAfterDiscount(double discount)
+        {
+            return Product.CalculatePriceAfterDiscount(discount);
+        }
+    }
+}
---
+++ FirstCoreMVCWebApplication/SOLID/Composition/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstCoreMVCWebApplication.SOLID.Composition
+{
+    public class Product
+    {
+        public double Price { get; set; }
+        public double CalculatePriceAfterDiscount(double discount)
+        {
+            return Price - (Price * discount) / 100;
+        }
+    }
+}
