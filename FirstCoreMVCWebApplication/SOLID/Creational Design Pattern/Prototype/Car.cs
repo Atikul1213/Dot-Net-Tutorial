99a14c2b8a090a7ce22b5f4d8abf1dd0e3118947
Atikul1213 <atikul.islam@brainstation-23.com>
2025-01-12 15:56:59 +0600

    Singleton and prototype desing pattern

---
+++ FirstCoreMVCWebApplication/SOLID/Creational Design Pattern/Prototype/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace FirstCoreMVCWebApplication.SOLID.Creational_Design_Pattern.Prototype
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public double Speed { get; set; }
+        public double Fuel { get; set; }
+
+        public Car Copy()
+        {
+            return new Car() { Fuel = this.Fuel, Model = this.Model, Speed = this.Speed };
+        }
+    }
+}
---
+++ FirstCoreMVCWebApplication/SOLID/Creational Design Pattern/Singleton/Logger.cs
@@ -0,0 +1,30 @@
+﻿namespace FirstCoreMVCWebApplication.SOLID.Creational_Design_Pattern.Singleton
+{
+    public class Logger
+    {
+        private const string FILE_PATH = "C:\\log.txt";
+        private static Logger _logger;
+        private Logger()
+        {
+
+        }
+        public static Logger GetLogger()
+        {
+            if (_logger == null)
+            {
+                _logger = new Logger();
+            }
+            return _logger;
+        }
+
+        public void WriteLog(string message)
+        {
+            lock (this)
+            {
+                File.WriteAllTextAsync(FILE_PATH, message);
+            }
+        }
+    }
+}
+
+// Logger logger = Logger.GetLogger();
