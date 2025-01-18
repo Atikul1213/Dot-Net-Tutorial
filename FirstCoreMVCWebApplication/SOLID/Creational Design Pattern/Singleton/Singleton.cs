cf24317f47a3afe5b612ebab97c01796ef9f8ff5
Atikul1213 <atikul.islam@brainstation-23.com>
2025-01-18 15:06:32 +0600

    single ton

---
+++ FirstCoreMVCWebApplication/SOLID/Creational Design Pattern/Singleton/Singleton.cs
@@ -0,0 +1,26 @@
+﻿namespace FirstCoreMVCWebApplication.SOLID.Creational_Design_Pattern.Singleton
+{
+    public class Singleton
+    {
+        private static volatile Singleton instance;
+        private static readonly object lockObject = new object();
+        private string data;
+
+        private Singleton(string data)
+        {
+            this.data = data;
+        }
+
+        public static Singleton getInsatance(string data)
+        {
+            lock (lockObject)
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton(data);
+                }
+                return instance;
+            }
+        }
+    }
+}
