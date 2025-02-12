bf27c25cd235e8d1b85232d1c5ac80aa7fab7a14
Atikul1213 <atikul.islam@brainstation-23.com>
2025-02-12 22:46:03 +0600

    Shallow and deep copy

---
+++ FirstCoreMVCWebApplication/SOLID/ShallowDeepCopy/ShallowCopy/Employee.cs
@@ -0,0 +1,19 @@
+﻿namespace FirstCoreMVCWebApplication.SOLID.ShallowDeepCopy.ShallowCopy
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public Address EmpAddress { get; set; }
+
+        public Employee GetClone()
+        {
+            return (Employee)this.MemberwiseClone();
+        }
+    }
+
+    public class Address
+    {
+        public string address { get; set; }
+    }
+}
