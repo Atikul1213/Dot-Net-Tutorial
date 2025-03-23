e4f01ddfc1a41056ff6b5c5f8e08f5dde3797057
Atikul1213 <atikul.islam@brainstation-23.com>
2025-03-23 23:53:08 +0600

    Fluent api validation

---
+++ FirstCoreMVCWebApplication/Controllers/FluentValidationController.cs
@@ -0,0 +1,34 @@
+﻿using FirstCoreMVCWebApplication.Models.Fluent_Validation.ProductModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstCoreMVCWebApplication.Controllers
+{
+    public class FluentValidationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] ProductCreateDTO productDto)
+        {
+            var validator = new ProductCreateDTOValidator();
+
+            var validationResult = validator.Validate(productDto);
+
+            if (!validationResult.IsValid)
+            {
+                var errorResponse = validationResult.Errors.Select(e => new
+                {
+                    Fields = e.PropertyName,
+                    Error = e.ErrorMessage
+                });
+
+                return BadRequest(new { Errors = errorResponse });
+            }
+
+            return View(productDto);
+        }
+    }
+}
---
+++ FirstCoreMVCWebApplication/Models/Fluent Validation/ProductModel/Tag.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstCoreMVCWebApplication.Models.Fluent_Validation.ProductModel
+{
+    public class Tag
+    {
+        public int TagId { get; set; }
+        public string Name { get; set; }
+        // Many-to-many relationship with Product
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
