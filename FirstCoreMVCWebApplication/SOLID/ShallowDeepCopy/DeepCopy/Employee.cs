027442133633ca143ac558b9caa07e09f8bad9a4
Atikul1213 <atikul.islam@brainstation-23.com>
2025-03-24 23:06:41 +0600

    fix

--- FirstCoreMVCWebApplication/SOLID/ShallowDeepCopy/DeepCopy/Employee.cs
+++ FirstCoreMVCWebApplication/SOLID/ShallowDeepCopy/DeepCopy/Employee.cs
@@ -5,11 +5,12 @@
         public string Name { get; set; }
         public string Department { get; set; }
         public Address EmpAddress { get; set; }
-        //public Employee GetClone()
-        //{
-        //    Employee employee = (Employee)this.MemberwiseClone();
-        //    employee.EmpAddress = EmpAddress.GetClone();
-        //}
+        public Employee GetClone()
+        {
+            Employee employee = (Employee)this.MemberwiseClone();
+            employee.EmpAddress = EmpAddress.GetClone();
+            return employee;
+        }
     }
     public class Address
     {
