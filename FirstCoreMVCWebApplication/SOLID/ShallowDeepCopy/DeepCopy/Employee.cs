4bf8ffc311cdcedd1719ac8e5f8768fc81a11faf
Atikul1213 <atikul.islam@brainstation-23.com>
2025-03-24 23:00:00 +0600

    fix error

--- FirstCoreMVCWebApplication/SOLID/ShallowDeepCopy/DeepCopy/Employee.cs
+++ FirstCoreMVCWebApplication/SOLID/ShallowDeepCopy/DeepCopy/Employee.cs
@@ -5,11 +5,11 @@
         public string Name { get; set; }
         public string Department { get; set; }
         public Address EmpAddress { get; set; }
-        public Employee GetClone()
-        {
-            Employee employee = (Employee)this.MemberwiseClone();
-            employee.EmpAddress = EmpAddress.GetClone();
-        }
+        //public Employee GetClone()
+        //{
+        //    Employee employee = (Employee)this.MemberwiseClone();
+        //    employee.EmpAddress = EmpAddress.GetClone();
+        //}
     }
     public class Address
     {
